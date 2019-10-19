2df94137013d894414b04754a680b6b6f09b8042
layonmartins <layonmartins03@gmail.com>
2019-10-18 22:19:55 -0300

    add the onClickFindBeer(View view) method

--- app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/hfad/beeradviser/FindBeerActivity.java
@@ -1,14 +1,33 @@
 package com.hfad.beeradviser;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+//import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class FindBeerActivity extends AppCompatActivity {
+public class FindBeerActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_beer);
+    }
+
+    //Called when the user clicks the button
+    public void onClickFindBeer(View view){
+
+        //Get a reference to the TextView
+        TextView brands = (TextView) findViewById(R.id.brands);
+
+        //Get a reference to the Spinner
+        Spinner color = (Spinner) findViewById(R.id.color);
+
+        //Get the selected item in the Spinner
+        String beerType = String.valueOf(color.getSelectedItem());
+
+        //Display the selected item
+        brands.setText(beerType);
     }
 }
